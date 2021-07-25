ed11381b71985c5e06fe7545cf68f9e72cbb7929
akashantal <56596777+akashantal@users.noreply.github.com>
2021-07-25 08:17:56 +0530

    Update pair.cpp

--- pair.cpp
+++ pair.cpp
@@ -30,3 +30,46 @@ cout<<"Pair Not Found";
 return 0;
 
 }
+
+//using sorting
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,i,j,sum,w=0;
+cout<<"Enter the size of array:";
+cin>>n;
+cout<<"\nEnter the sum to be find:";
+cin>>sum;
+cout<<"\n Enter Array:\n";
+int arr[n];
+for(i=0;i<n;i++)
+        {
+                cin>>arr[i];
+        }
+sort(arr,arr+n);
+cout<<"Sorted array is: ";
+for(i=0;i<n;i++)
+        {
+                cout<<arr[i]<<" ";
+        }
+
+for(i=0;i<n && arr[i]<sum;i++)
+        {
+                for(j=i+i;j<n&&arr[j]<sum;j++)
+                        {
+                                if(arr[i]+arr[j]==sum)
+                                        {
+                                                cout<<"\nPair found: ("<<arr[i]<<" ,"<<arr[j]<<" ) \n";
+                                                w++;
+                                        }
+                        }
+
+        }
+if(w==0)
+cout<<"Pair Not Found";
+return 0;
+}
+
+
